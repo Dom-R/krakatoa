8cf8af1b5a4cc7e208cefae6af319b75e04b15c2
Dom <dominik.reller@gmail.com>
2016-10-06 19:01:37 -0300

    Versão Inicial

---
+++ src/ast/KraClass.java
@@ -0,0 +1,21 @@
+package ast;
+/*
+ * Krakatoa Class
+ */
+public class KraClass extends Type {
+
+   public KraClass( String name ) {
+      super(name);
+   }
+
+   public String getCname() {
+      return getName();
+   }
+
+   private String name;
+   private KraClass superclass;
+   private InstanceVariableList instanceVariableList;
+   // private MethodList publicMethodList, privateMethodList;
+   // m�todos p�blicos get e set para obter e iniciar as vari�veis acima,
+   // entre outros m�todos
+}
