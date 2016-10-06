d052f12d7c65c677365a54e1d91c557974a73dda
Dom <dominik.reller@gmail.com>
2016-10-06 19:03:28 -0300

    Agora sim é inicial

--- src/ast/KraClass.java
+++ src/ast/KraClass.java
@@ -3,19 +3,19 @@ package ast;
  * Krakatoa Class
  */
 public class KraClass extends Type {
-
+	
    public KraClass( String name ) {
       super(name);
    }
-
+   
    public String getCname() {
       return getName();
    }
-
+   
    private String name;
    private KraClass superclass;
    private InstanceVariableList instanceVariableList;
    // private MethodList publicMethodList, privateMethodList;
-   // mï¿½todos pï¿½blicos get e set para obter e iniciar as variï¿½veis acima,
-   // entre outros mï¿½todos
+   // métodos públicos get e set para obter e iniciar as variáveis acima,
+   // entre outros métodos
 }
