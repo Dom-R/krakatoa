6e8e1b0ea376044bf5292e4411406e58067f4f31
Luan Maia <luangmaiadias@gmail.com>
2016-11-01 23:17:25 -0200

    Trocando printIdent do ParenthesisExpr quando vai imprimir ")"

--- src/ast/ParenthesisExpr.java
+++ src/ast/ParenthesisExpr.java
@@ -9,7 +9,7 @@ public class ParenthesisExpr extends Expr {
     public void genC( PW pw, boolean putParenthesis ) {
         pw.print("(");
         expr.genC(pw, false);
-        pw.printIdent(")");
+        pw.print(")");
     }
     
     public Type getType() {
@@ -21,7 +21,7 @@ public class ParenthesisExpr extends Expr {
 		// TODO Auto-generated method stub
     	pw.print("(");
         expr.genKra(pw, false);
-        pw.printIdent(")");
+        pw.print(")");
 	}
     
     private Expr expr;
